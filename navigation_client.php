c4e070ba70f97c60c18490ff049258d4a3214dbc
rkylems <85321672+rkylems@users.noreply.github.com>
2021-06-04 03:11:25 +0100

    Add files via upload

---
+++ navigation_client.php
@@ -0,0 +1,20 @@
+ 	<!-- NAVIGATION -->
+		<nav id="navigation">
+			<!-- container -->
+			<div class="container">
+				<!-- responsive-nav -->
+				<div id="responsive-nav">
+					<!-- NAV -->
+					<ul class="main-nav nav navbar-nav">
+						<li class="active"><a href="index.php">Accueil</a></li>
+						<li><a href="index.php?historique=''">mes Commandes</a></li>
+						<li><a href="index.php?client_maj=''">Maj Votre compte</a></li>
+						<li><a href="index.php?client_supp=''">supprimer Votre compte</a></li>
+					</ul>
+					<!-- /NAV -->
+				</div>
+				<!-- /responsive-nav -->
+			</div>
+			<!-- /container -->
+		</nav>
+		<!-- /NAVIGATION -->
